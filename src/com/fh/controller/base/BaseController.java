e0bb377c815cc3959f8b0431384fdfb51f1cc283
silenceNo1 <13161618211@163.com>
2018-02-22 20:25:32 +0800

    test

--- src/com/fh/controller/base/BaseController.java
+++ src/com/fh/controller/base/BaseController.java
@@ -1,6 +1,5 @@
 package com.fh.controller.base;
 
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.context.request.RequestContextHolder;
@@ -13,60 +12,70 @@ import com.fh.util.PageData;
 import com.fh.util.UuidUtil;
 
 /**
- * @author Silence13161618211
- * 修改时间：2015、12、11
+ * @author Silence13161618211 修改时间：2015、12、11
  */
 public class BaseController {
-	
+
 	protected Logger logger = Logger.getLogger(this.getClass());
 
 	private static final long serialVersionUID = 6357869213649815390L;
-	
-	/** new PageData对象
+
+	/**
+	 * new PageData对象
+	 * 
 	 * @return
 	 */
-	public PageData getPageData(){
+	public PageData getPageData() {
 		return new PageData(this.getRequest());
 	}
-	
-	/**得到ModelAndView
+
+	/**
+	 * 得到ModelAndView
+	 * 
 	 * @return
 	 */
-	public ModelAndView getModelAndView(){
+	public ModelAndView getModelAndView() {
 		return new ModelAndView();
 	}
-	
-	/**得到request对象
+
+	/**
+	 * 得到request对象
+	 * 
 	 * @return
 	 */
 	public HttpServletRequest getRequest() {
-		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+				.getRequest();
 		return request;
 	}
 
-	/**得到32位的uuid
+	/**
+	 * 得到32位的uuid
+	 * 
 	 * @return
 	 */
-	public String get32UUID(){
+	public String get32UUID() {
 		return UuidUtil.get32UUID();
 	}
-	
-	/**得到分页列表的信息
+
+	/**
+	 * 得到分页列表的信息
+	 * 
 	 * @return
 	 */
-	public Page getPage(){
+	public Page getPage() {
 		return new Page();
 	}
-	
-	public static void logBefore(Logger logger, String interfaceName){
+
+	public static void logBefore(Logger logger, String interfaceName) {
 		logger.info("");
 		logger.info("start");
 		logger.info(interfaceName);
 	}
-	
-	public static void logAfter(Logger logger){
+
+	public static void logAfter(Logger logger) {
 		logger.info("end");
 		logger.info("");
 	}
-	
+
 }
